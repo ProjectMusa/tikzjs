c138a9d44744bfabd99e4d46f5f64614a80d5652
ldXiao <xld704@gmail.com>
2024-01-23 15:09:58 -0500

    sample add

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ global.document = window.document
 import { TikzRoot } from './parser/TikzRoot'
 import { parse } from './parser/_tikzjs'
 import { generator_svg } from './generators/Generator'
-
+import * as fs from 'fs'
 export function runWorker(s: string): Object {
   return parse(s, {}) as TikzRoot
 }
 
 if (require.main === module) {
   // Uncomment to compile ./test/test.btx to ./test/test.html
-  const ast = runWorker('\\tikz[]{\\path[draw](0,0)-|++(1,1 cm)--++(2,2cm);}')
+  let strTikz = fs.readFileSync('./sample/test.tikz', 'utf8')
+  const ast = runWorker(strTikz)
   const result = generator_svg.generate(ast as TikzRoot)
   console.log(JSON.stringify(result))
   console.log(result[0].outerHTML)
+  fs.writeFileSync('./sample/test.svg', result[0].outerHTML)
 }
