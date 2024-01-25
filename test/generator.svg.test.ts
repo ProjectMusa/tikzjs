268cfaacb737985b2ba1c54e4bf641bd49e028d1
ldXiao <xld704@gmail.com>
2024-01-25 17:53:06 -0500

    add node test

--- test/generator.svg.test.ts
+++ test/generator.svg.test.ts
@@ -8,7 +8,6 @@ import { TikzRoot } from '../src/parser/TikzRoot'
 test('svg_path_-|_test', () => {
   const ast = runWorker('\\tikz[]{\\path[draw](0,0)-|++(1,1 cm)--++(2,2cm);}')
   const result = generator_svg.generate(ast as TikzRoot)
-  console.log(JSON.stringify(result))
   console.log(result[0].outerHTML)
   expect(result)
 })
@@ -16,7 +15,6 @@ test('svg_path_-|_test', () => {
 test('svg_grid_test', () => {
   const ast = runWorker('\\tikz[]{\\path[draw](0,0) grid (1,1 cm) -- ++(2,2cm);}')
   const result = generator_svg.generate(ast as TikzRoot)
-  console.log(JSON.stringify(result))
   console.log(result[0].outerHTML)
   expect(result)
 })
@@ -24,7 +22,6 @@ test('svg_grid_test', () => {
 test('svg_quadratic_bezier_test', () => {
   const ast = runWorker('\\tikz[]{\\path[draw](0,0) .. controls (1,1 cm) .. ++(2,0cm);}')
   const result = generator_svg.generate(ast as TikzRoot)
-  console.log(JSON.stringify(result))
   console.log(result[0].outerHTML)
   expect(result)
 })
@@ -32,7 +29,13 @@ test('svg_quadratic_bezier_test', () => {
 test('svg_cubic_bezier_test', () => {
   const ast = runWorker('\\tikz[]{\\path[draw](0,0) .. controls (1,1 cm) and (2,1) .. ++(3,0cm);}')
   const result = generator_svg.generate(ast as TikzRoot)
-  console.log(JSON.stringify(result))
+  console.log(result[0].outerHTML)
+  expect(result)
+})
+
+test('svg_node_latex_test', () => {
+  const ast = runWorker('\\tikz[]{\\path[draw](0,0) .. controls (1,1 cm) and (2,1) .. node[]{$x^2$} ++(3,0cm);}')
+  const result = generator_svg.generate(ast as TikzRoot)
   console.log(result[0].outerHTML)
   expect(result)
 })
